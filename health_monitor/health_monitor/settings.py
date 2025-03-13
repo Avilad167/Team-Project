519f24661a791e47e0df930c4b373d785fdb9e4e
Avi Lad <146333928+Avilad167@users.noreply.github.com>
2025-03-13 18:39:34 +0000

    Update settings.py

--- health_monitor/health_monitor/settings.py
+++ health_monitor/health_monitor/settings.py
@@ -121,11 +121,4 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# Email backend settings for Gmail
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'lbuteamproject@gmail.com'
-EMAIL_HOST_PASSWORD = 'team_project25'
+
